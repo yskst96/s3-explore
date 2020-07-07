3cd1e6c07cc6765663a861668a51dedc224648c2
yusukesata <yskstdev@gmail.com>
2020-07-08 00:42:52 +0900

    s3listを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-import { s3list, s3get } from "./aws"
+import { s3list } from './aws';
+import { S3Object } from './Composition';
 
-function App() {
+const App: React.FC = () => {
+  const [list, setList] = useState([] as S3Object[]);
+  const [current, setCurrent] = useState('');
 
-  const key = "foo.csv"
+  useEffect(() => {
+    let unmounted = false;
+
+    (async () => {
+      const list = await s3list();
+
+      if (!unmounted) {
+        setList(list);
+      }
+    })();
+
+    return () => {
+      unmounted = true;
+    };
+  }, []);
 
   return (
-    <div className= "App" >
-    <button onClick={ s3list }> list </button>
-      <button onClick={()=> { s3get(key)} }> get </button>
+    <div className='App'>
+      <div>current:{current ? current : '/'}</div>
+      {list.map((o) => {
+        return <div key={o.key}>{o.key}</div>;
+      })}
     </div>
   );
-}
+};
 
 export default App;
-
