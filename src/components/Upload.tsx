28417cfa1533e419af8f2b079aba517332ebeba9
yusukesata <yskstdev@gmail.com>
2020-07-10 01:39:27 +0900

    [WIP]アップロード機能

---
+++ src/components/Upload.tsx
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react';
+import { s3put } from '../aws';
+
+type UploadProp = {
+  current: string;
+};
+
+const Upload: React.FC<UploadProp> = ({ current }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onClick = () => {
+    const input = inputRef.current;
+    if (input) {
+      console.log(current);
+      input.click();
+    }
+  };
+  const upload = () => {};
+
+  return (
+    <div>
+      <button onClick={onClick}>upload</button>
+      <input id='upload-form' className='upload-form' type='file' onChange={upload} ref={inputRef} />
+    </div>
+  );
+};
+
+export { Upload };
